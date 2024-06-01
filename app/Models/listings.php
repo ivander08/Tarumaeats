600d74098bb7664a476eb3a7424375ce8a2a8eea
willsenyogi <willsenyp21@gmail.com>
2024-06-01 20:36:48 +0700

    created listings database model

---
+++ app/Models/listings.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class listings extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'location_name',
+        'location_address',
+        'price_range',
+        'website',
+        'phone_number',
+        'email',
+        'latitude',
+        'longitude',
+        'images',
+        'tags',
+        'special_features',
+        'price_per_person',
+        'payment_options',
+        'open_hours',
+        'closed_hours',
+    ];
+
+
+    protected $casts = [
+        'latitude' => 'string',
+        'longitude' => 'string',
+        'tags' => 'array',
+        'special_features' => 'array',
+        'payment_options' => 'array',
+    ];
+
+    protected $table = 'listings';
+}
