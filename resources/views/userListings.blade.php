a7c160f188b80a63aee28c465eed6c2133654945
ivander08 <ivanderseah08@gmail.com>
2024-06-05 21:06:05 +0700

    Made basic table

--- resources/views/userListings.blade.php
+++ resources/views/userListings.blade.php
@@ -15,6 +15,40 @@
         <button type="create" class="user-listings-create">Create Listing</button>
     </div>
     <div class="user-listings-table">
-        
+        <table>
+            <thead>
+                <th>Name</th>
+                <th>Rating</th>
+                <th>Status</th>
+                <th>Approval</th>
+                <th>Last Modified</th>
+                <th>
+                    <input type="text" placeholder="Search Name...">
+                </th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>DLS CKN</td>
+                    <td>5.0 (3)</td>
+                    <td>
+                        <div class="listing-status-active">
+                            &#x2022; Active
+                        </div>
+                    </td>
+                    <td>
+                        <div class="listing-status-approved">
+                            &#x2022; Approved
+                        </div>
+                    </td>
+                    <td>3 minutes ago</td>
+                    <td>
+                        <div class="user-listings-table-interact">
+                            <button class="user-listings-table-interact-btn">Edit</button>
+                            <button class="user-listings-table-interact-btn">Delete</button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 @endsection
