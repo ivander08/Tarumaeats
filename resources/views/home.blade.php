f7741d0077b1faa3df5f5f987f6e8d3654e6160b
willsenyogi <willsenyp21@gmail.com>
2024-06-17 16:43:43 +0700

    show featured locations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,6 +25,59 @@
                 <h1>Featured Eats</h1>
                 <p>The best eats recommended by us.</p>
             </div>
+            <div class="eats-results-container">
+                @php
+                    if (!function_exists('PriceRangeDisplay')) {
+                        function PriceRangeDisplay($price_range)
+                        {
+                            switch ($price_range) {
+                                case 'under_price':
+                                    return '&lt;Rp10,000';
+                                case 'thirty_price':
+                                    return 'Rp10,000 - Rp30,000';
+                                case 'sixty_price':
+                                    return 'Rp30,000 - Rp60,000';
+                                case 'over_price':
+                                    return '&gt;Rp60,000';
+                                default:
+                                    return 'Price range not specified';
+                            }
+                        }
+                    }
+                @endphp
+                @foreach ($listings as $listing)
+                    <div class="eats-cards-container">
+                        <a href="{{ route('eats.show', $listing->id) }}">
+                            <div class="eats-card-image"
+                                style="background-image: url('data:image/jpeg;base64,{{ $listing->banner_image }}');">
+                            </div>
+                        </a>
+                        <div class="eats-card-content">
+                            <div class="eats-card-name-rating">
+                                <h1>{{ $listing->location_name }}</h1>
+                                <div class="eats-card-rating">
+                                    <img src="{{ asset('images/star.svg') }}" alt="Star Icon"
+                                        class="eats-card-star-icon">
+                                    @php
+                                        $ratingsCount = optional($listing->ratings)->count() ?: 0;
+                                        $averageRating =
+                                            $ratingsCount > 0
+                                                ? number_format(optional($listing->ratings)->avg('rating'), 1, '.', '')
+                                                : 0;
+                                    @endphp
+                                    @if ($ratingsCount > 0)
+                                        <h3>{{ $averageRating }} ({{ $ratingsCount }})</h3>
+                                    @else
+                                        <h3>No ratings yet</h3>
+                                    @endif
+                                </div>
+                            </div>
+                            <p>{!! PriceRangeDisplay($listing->price_range) !!}</p>
+                            <h2>{{ $listing->location_address }}</h2>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 
