58702593caf26fd32497d1916f2161912dc4a214
Justin Salim <justinsalim73@gmail.com>
2024-06-24 22:27:05 +0700

    Menambahkan komen pada userDetails.blade.php

--- resources/views/user/userDetails.blade.php
+++ resources/views/user/userDetails.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'User Details')
 
 @section('content')
+<!-- Bagian untuk menampilkan detail user -->
     <div class="user-listings-head">
         <div class="user-listings-head-text">
             <div class="user-listings-head-text-settings">
