fa8833a7bbedf42d3a584a3baf520c0f834c8f7a
Justin Salim <justinsalim73@gmail.com>
2024-06-24 22:13:34 +0700

    Menambahkan komen pada RatingsController.php

--- app/Http/Controllers/RatingsController.php
+++ app/Http/Controllers/RatingsController.php
@@ -7,34 +7,27 @@ use App\Models\Ratings;
 
 class RatingsController extends Controller
 {
+    // Fungsi store digunakan untuk menyimpan data rating
     public function store(Request $request)
     {
-        // Validate the request
         $request->validate([
             'location_name' => 'required|string',
             'rating' => 'required|integer|min:1|max:5',
         ]);
-
-        // Check if the user has already rated the location
         $existingRating = Ratings::where([
             'name' => auth()->user()->name,
             'location_name' => $request->location_name,
         ])->first();
 
-        // If the user has already rated and the new rating is the same, delete the rating
         if ($existingRating && $existingRating->rating == $request->rating) {
             $existingRating->delete();
         } else {
-            // If the user has already rated but the new rating is different, update the rating
             if ($existingRating) {
                 $existingRating->update(['rating' => $request->rating]);
             } else {
-                // Generate a unique ID for the rating
                 do {
                     $ratingId = random_int(1, PHP_INT_MAX);
                 } while (Ratings::find($ratingId) !== null);
-
-                // If the user has not rated yet, create a new rating
                 Ratings::create([
                     'id' => $ratingId,
                     'name' => auth()->user()->name,
