ee8112fbe6946d568390488f93805b6755be1799
Justin Salim <justinsalim73@gmail.com>
2024-06-24 22:16:08 +0700

    Menambahkan komen pada PasswordController.php

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -11,7 +11,7 @@ use Illuminate\Validation\Rules\Password;
 class PasswordController extends Controller
 {
     /**
-     * Update the user's password.
+     * Memperbarui password pengguna.
      */
     public function update(Request $request): RedirectResponse
     {
