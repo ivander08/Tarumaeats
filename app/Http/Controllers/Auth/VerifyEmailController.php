3db1d7cb9c31e64c19ed58656684a4edead1fb0b
Justin Salim <justinsalim73@gmail.com>
2024-06-24 22:16:39 +0700

    Menambahkan komen pada VerifyEmailController.php

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\RedirectResponse;
 class VerifyEmailController extends Controller
 {
     /**
-     * Mark the authenticated user's email address as verified.
+     * Menandai email pengguna sebagai terverifikasi.
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
