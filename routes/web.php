bc03d872e37168de04e1513be4c724451ecdfb4f
Justin Salim <justinsalim73@gmail.com>
2024-06-24 20:43:21 +0700

    Add comment and delete unuse code

--- routes/web.php
+++ routes/web.php
@@ -23,50 +23,46 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/', [ListingsController::class, 'home'])->name('home');
-Route::get('/eats', [ListingsController::class, 'indexApproved'])->name('eats');
-Route::post('/eats/filter', [ListingsController::class, 'filter'])->name('eats.filter');
-Route::get('/eats/{id}', [ListingsController::class, 'show'])->name('eats.show');
-Route::post('/ratings/store', [RatingsController::class, 'store'])->name('ratings.store');
+Route::get('/', [ListingsController::class, 'home'])->name('home'); // Rute untuk halaman utama
+Route::get('/eats', [ListingsController::class, 'indexApproved'])->name('eats'); // Rute untuk menampilkan listing yang telah disetujui
+Route::post('/eats/filter', [ListingsController::class, 'filter'])->name('eats.filter'); // Rute untuk melakukan filter pada listing
+Route::get('/eats/{id}', [ListingsController::class, 'show'])->name('eats.show'); // Rute untuk menampilkan detail listing
+Route::post('/ratings/store', [RatingsController::class, 'store'])->name('ratings.store'); // Rute untuk menyimpan rating
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); // Rute untuk halaman dashboard, memerlukan autentikasi dan verifikasi
 
 Route::middleware('auth')->group(function () {
-    Route::get('/user/details', [UserController::class, 'show'])->name('user');
-    Route::put('/user/details/update', [UserController::class, 'update'])->name('user.update');
-    Route::delete('/user/details/delete', [UserController::class, 'delete'])->name('user.delete');
+    Route::get('/user/details', [UserController::class, 'show'])->name('user'); // Rute untuk menampilkan detail pengguna
+    Route::put('/user/details/update', [UserController::class, 'update'])->name('user.update'); // Rute untuk memperbarui detail pengguna
+    Route::delete('/user/details/delete', [UserController::class, 'delete'])->name('user.delete'); // Rute untuk menghapus pengguna
     
 
-    Route::get('/user/listings', [ListingsController::class, 'index'])->name('listings');
-    Route::get('/user/listings/create', [ListingsController::class, 'create'])->name('listings.create');
-    Route::get('user/listings/edit/{id}', [ListingsController::class, 'edit'])->name('listings.edit');
-    Route::post('user/listings/store', [ListingsController::class, 'store'])->name('listings.store');
-    Route::put('user/listings/update/{id}', [ListingsController::class, 'update'])->name('listings.update');
-    Route::delete('user/listings/destroy/{id}', [ListingsController::class, 'destroy'])->name('listings.destroy');
-    Route::get('user/listings/search', [ListingsController::class, 'search'])->name('listings.search');
-    Route::post('user/listings/updateStatus', [ListingsController::class, 'updateStatus'])->name('listings.updateStatus');
-    Route::get('/user/listings/preview/{id}', [ListingsController::class, 'preview'])->name('listings.preview');
-
-    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/user/listings', [ListingsController::class, 'index'])->name('listings'); // Rute untuk menampilkan semua listing pengguna
+    Route::get('/user/listings/create', [ListingsController::class, 'create'])->name('listings.create'); // Rute untuk membuat listing baru
+    Route::get('user/listings/edit/{id}', [ListingsController::class, 'edit'])->name('listings.edit'); // Rute untuk mengedit listing
+    Route::post('user/listings/store', [ListingsController::class, 'store'])->name('listings.store'); // Rute untuk menyimpan listing baru
+    Route::put('user/listings/update/{id}', [ListingsController::class, 'update'])->name('listings.update'); // Rute untuk memperbarui listing
+    Route::delete('user/listings/destroy/{id}', [ListingsController::class, 'destroy'])->name('listings.destroy'); // Rute untuk menghapus listing
+    Route::get('user/listings/search', [ListingsController::class, 'search'])->name('listings.search'); // Rute untuk mencari listing
+    Route::post('user/listings/updateStatus', [ListingsController::class, 'updateStatus'])->name('listings.updateStatus'); // Rute untuk memperbarui status listing
+    Route::get('/user/listings/preview/{id}', [ListingsController::class, 'preview'])->name('listings.preview'); // Rute untuk preview listing
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/admin/listings', [AdminController::class, 'listingsIndex'])->name('admin.listings');
-    Route::get('/admin/users', [AdminController::class, 'usersIndex'])->name('admin.users');
-    Route::get('/admin/users/{user}/edit', [AdminController::class, 'editUser'])->name('admin.users.edit');
-    Route::put('/admin/users/{user}/update', [AdminController::class, 'updateUser'])->name('admin.users.update');
-    Route::delete('/admin/users/{user}', [AdminController::class, 'destroyUser'])->name('admin.users.destroy');
-    Route::get('admin/listings/edit/{id}', [AdminController::class, 'editListing'])->name('admin.listings.edit');
-    Route::put('admin/listings/update/{id}', [AdminController::class, 'updateListing'])->name('admin.listings.update');
-    Route::post('/admin/listings/updateStatus', [AdminController::class, 'updateStatus'])->name('admin.listings.updateStatus');
-    Route::post('/admin/listings/updateApproval', [AdminController::class, 'updateApproval'])->name('admin.listings.updateApproval');
-    Route::post('/admin/listings/updateFeatured', [AdminController::class, 'updateFeatured'])->name('admin.listings.updateFeatured');
-    Route::post('/admin/users/updateRole', [AdminController::class, 'updateRole'])->name('admin.user.updateRole');
-    Route::get('/admin/listings/preview/{id}', [AdminController::class, 'show'])->name('admin.preview');
+    Route::get('/admin/listings', [AdminController::class, 'listingsIndex'])->name('admin.listings'); // Rute untuk menampilkan semua listing di halaman admin
+    Route::get('/admin/users', [AdminController::class, 'usersIndex'])->name('admin.users'); // Rute untuk menampilkan semua pengguna di halaman admin
+    Route::get('/admin/users/{user}/edit', [AdminController::class, 'editUser'])->name('admin.users.edit'); // Rute untuk mengedit pengguna di halaman admin
+    Route::put('/admin/users/{user}/update', [AdminController::class, 'updateUser'])->name('admin.users.update'); // Rute untuk memperbarui pengguna di halaman admin
+    Route::delete('/admin/users/{user}', [AdminController::class, 'destroyUser'])->name('admin.users.destroy'); // Rute untuk menghapus pengguna di halaman admin
+    Route::get('admin/listings/edit/{id}', [AdminController::class, 'editListing'])->name('admin.listings.edit'); // Rute untuk mengedit listing di halaman admin
+    Route::put('admin/listings/update/{id}', [AdminController::class, 'updateListing'])->name('admin.listings.update'); // Rute untuk memperbarui listing di halaman admin
+    Route::post('/admin/listings/updateStatus', [AdminController::class, 'updateStatus'])->name('admin.listings.updateStatus'); // Rute untuk memperbarui status listing di halaman admin
+    Route::post('/admin/listings/updateApproval', [AdminController::class, 'updateApproval'])->name('admin.listings.updateApproval'); // Rute untuk memperbarui status persetujuan listing di halaman admin
+    Route::post('/admin/listings/updateFeatured', [AdminController::class, 'updateFeatured'])->name('admin.listings.updateFeatured'); // Rute untuk memperbarui status featured listing di halaman admin
+    Route::post('/admin/users/updateRole', [AdminController::class, 'updateRole'])->name('admin.user.updateRole'); // Rute untuk memperbarui peran pengguna di halaman admin
+    Route::get('/admin/listings/preview/{id}', [AdminController::class, 'show'])->name('admin.preview'); // Rute untuk preview listing di halaman admin
 });
 
 require __DIR__ . '/auth.php';
